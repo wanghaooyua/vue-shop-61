c7586320a48abdc9b270003c055363305bc6b37f
wanghaooyua <851037351@qq.com>
2019-02-15 19:44:20 +0800

    后台首页面开发完毕

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,118 @@
 <template>
-    <div>
-        <h2>后台首页面</h2>
-    </div>
+  <el-container>
+    <el-header>
+      <div class="logo-box">
+        <img src="../assets/img/heima.png" alt>
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="loginout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="menushow ? '65px' :'200px'">
+        <div
+          style="height:25px;background-color:rgb(74,80,100);text-align:center;color:#fff;line-height:25px;
+        font-size:12px;letter-spacing:0.1em;cursor:pointer;user-select:none"
+          @click="menushow=!menushow"
+        >|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :style="menushow ? 'width:65px' : 'width:200px'"
+          :collapse="menushow"
+          :collapse-transition="false"
+          :unique-opened="true"
+        >
+          <el-submenu :index="item.id+''" v-for="(item,k) in menuList" :key="item.id">
+            <template slot="title">
+              <i :class="'iconfont icon-'+menuicon[k]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+
+            <el-menu-item
+              :index="item.id+'-'+item2.id"
+              v-for="item2 in item.children"
+              :key="item2.id"
+            >
+            <i class="el-icon-menu"></i>
+            <span>{{item2.authName}}</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+          <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  created() {
+    // 获取左侧功能数据按钮
+    this.getMenuList()
+  },
+  data() {
+    return {
+      menushow: false, // 设定左侧菜单打开收起
+      menuList: [], // 接收左侧功能按钮数据
+      menuicon: ['users', 'tijikongjian', 'shangpin', 'danju', 'baobiao']
+    }
+  },
+  methods: {
+    loginout() {
+      this.$confirm('是否确认退出登录?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          window.sessionStorage.removeItem('token')
+          this.$router.push('/login')
+        })
+        .catch(() => {})
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.menuList = res.data
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.el-container {
+  height: 100%;
+  .el-header {
+    background-color: rgb(55, 61, 65);
+    padding: 0;
+    padding-right: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between; //盒子两端对齐
+    .logo-box {
+      font-size: 22px;
+      color: #fff;
+      display: flex;
+      align-items: center; //定义flex子项在flex容器的当前行的侧轴（纵轴）方向上的对齐方式
+      user-select: none; //禁止可以被选中
+      img {
+        height: 50px;
+        width: 50px;
+        margin-right: 10px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
+import Welcome from '@/components/Welcome'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
     {
       path: '/login',
@@ -13,7 +14,27 @@ export default new Router({
     },
     {
       path: '/home',
-      component: Home
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          component: Welcome
+        }
+      ]
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    return next()
+  }
+
+  var token = window.sessionStorage.getItem('token')
+  if (!token) {
+    return next('/login')
+  }
+  next()
+})
+export default router
